78d41b7bfa8de123f4418fac3cbe46c2dd0e8eac
Aditya <adityakulkarni819@gmail.com>
2022-11-04 15:18:12 +0530

    removed assignment folder

--- 31147_LP1_Assignment/DS2/DS2/client.py
+++
@@ -1,5 +0,0 @@
-import xmlrpc.client
-proxy = xmlrpc.client.ServerProxy('http://localhost:7500/')
-print("factorial of 3 is : %s" % str(proxy.factorial_rpc(3)))
-print("factorial of 5 is : %s" % str(proxy.factorial_rpc(5)))
-print("square of 15 is : %s" % str(proxy.square_rpc(15)))
